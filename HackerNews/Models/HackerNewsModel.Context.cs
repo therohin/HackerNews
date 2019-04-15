e0c6aab67b20eb550d2c8721a14e445b06ec706a
Rohin <rohintak@gmail.com>
2019-04-15 19:04:42 +0530

    DB Updated

--- HackerNews/Models/HackerNewsModel.Context.cs
+++ HackerNews/Models/HackerNewsModel.Context.cs
@@ -16,7 +16,7 @@ namespace HackerNews.Models
     public partial class Entities : DbContext
     {
         public Entities()
-            : base("name=Entities")
+            : base("name=HackerNewsEntities")
         {
         }
     
